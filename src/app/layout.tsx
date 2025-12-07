f928df4af334be11bc8763c54cd400ca2d73dd6b
torontopubliclibra <dana.r.teagle@gmail.com>
2025-12-07 13:42:03 -0500

    chore: metadata improvements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
   description:
     "Navigating name and gender marker changes can feel overwhelming, but I.D. Guide is here to help—offering step-by-step guides, downloadable forms, and resources to support you every step of the way.",
   keywords: [
-    "name change", "gender marker", "identity documents", "Ontario", "Canada", "trans", "non-binary", "Two-Spirit", "legal forms", "passport", "health card", "birth certificate", "driver's license", "permanent resident", "social insurance", "CRA", "LGBTQ", "step-by-step guides", "accessible resources"
+    "name change", "gender marker", "sex marker", "legal name change", "identity documents", "Canada", "Ontario", "Alberta", "British Columbia", "Quebec", "Saskatchewan", "Manitoba", "Nova Scotia", "New Brunswick", "trans", "trans technology", "non-binary", "Two-Spirit", "legal forms", "passport", "health card", "birth certificate", "driver's license", "permanent resident", "permanent residency", "social insurance", "CRA", "LGBTQ", "step-by-step guides", "accessible resources"
   ],
   openGraph: {
     title: "I.D. Guide | Helping you navigate name and gender marker changes",
@@ -18,10 +18,10 @@ export const metadata: Metadata = {
     siteName: "I.D. Guide",
     images: [
       {
-        url: "https://idguide.ca/og-image.png",
+        url: "https://idguide.ca/stacks.jpg",
         width: 1200,
         height: 630,
-        alt: "I.D. Guide logo and tagline",
+        alt: "Stacks of paperwork with colourful paperclips",
       },
     ],
     locale: "en_CA",
@@ -32,9 +32,9 @@ export const metadata: Metadata = {
     title: "I.D. Guide | Helping you navigate name and gender marker changes",
     description:
       "Navigating name and gender marker changes can feel overwhelming, but I.D. Guide is here to help—offering step-by-step guides, downloadable forms, and resources to support you every step of the way.",
-    site: "@danateagle",
-    creator: "@danateagle",
-    images: ["https://idguide.ca/og-image.png"],
+    site: "https://danateagle.com",
+    creator: "Dana Rosamund Teagle",
+    images: ["https://idguide.ca/stacks.jpg"],
   },
   alternates: {
     canonical: "https://idguide.ca/",
