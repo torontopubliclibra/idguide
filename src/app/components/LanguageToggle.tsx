2ff2fe2cf5cecbefa4458a735b8ff28ab93a4771
torontopubliclibra <dana.r.teagle@gmail.com>
2026-01-19 16:10:56 -0500

    feat: Added French translation beta

--- src/app/components/LanguageToggle.tsx
+++ src/app/components/LanguageToggle.tsx
@@ -1,42 +1,43 @@
 "use client";
 
-import { useEffect, useState } from "react";
-
 export function LanguageToggle({ locale }: { locale: string }) {
-  const [toEnglish, setToEnglish] = useState("/");
-  const [toFrench, setToFrench] = useState("/");
-
-  useEffect(() => {
+  const getRedirectUrl = (lang: string) => {
     if (typeof window !== "undefined") {
       const { protocol, hostname, port, pathname, search, hash } = window.location;
       const baseHost = hostname.replace(/^(fr\.)+/, "");
       const portPart = port ? `:${port}` : "";
-      // eslint-disable-next-line react-hooks/set-state-in-effect
-      setToEnglish(`${protocol}//${baseHost}${portPart}${pathname}${search}${hash}`);
-      setToFrench(`${protocol}//fr.${baseHost}${portPart}${pathname}${search}${hash}`);
+      if (lang === "en") {
+        return `${protocol}//${baseHost}${portPart}${pathname}${search}${hash}`;
+      } else {
+        return `${protocol}//fr.${baseHost}${portPart}${pathname}${search}${hash}`;
+      }
     }
-  }, []);
+    return "/";
+  };
 
-  const handleLanguageClick = (lang: string) => {
+  const handleLanguageClick = (lang: string, event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
     if (typeof window !== "undefined") {
       localStorage.setItem("preferredLanguage", lang);
+      const url = getRedirectUrl(lang);
+      window.location.assign(url);
     }
   };
 
   return (
     <span>
       <a
-        href={toEnglish}
+        href={getRedirectUrl("en")}
         style={{ textDecoration: locale === "en" ? "underline" : undefined }}
-        onClick={() => handleLanguageClick("en")}
+        onClick={e => handleLanguageClick("en", e)}
       >
         EN
       </a>
       {" / "}
       <a
-        href={toFrench}
+        href={getRedirectUrl("fr")}
         style={{ textDecoration: locale === "fr" ? "underline" : undefined }}
-        onClick={() => handleLanguageClick("fr")}
+        onClick={e => handleLanguageClick("fr", e)}
       >
         FR
       </a>
